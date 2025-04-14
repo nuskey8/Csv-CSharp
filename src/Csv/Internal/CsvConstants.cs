91344ee32ebdca28475da58dc5785ad1f460e162
nuskey8 <84110981+nuskey8@users.noreply.github.com>
2025-04-15 00:06:49 +0900

    Run formatter

--- src/Csv/Internal/CsvConstants.cs
+++ src/Csv/Internal/CsvConstants.cs
@@ -7,7 +7,6 @@ internal static class CsvConstants
     public const int FalseBytesLittleEndian = ((byte)'F') | ((byte)'a' << 8) | ((byte)'l' << 16) | ((byte)'s' << 24);
     public const int falseBytesLittleEndian = ((byte)'f') | ((byte)'a' << 8) | ((byte)'l' << 16) | ((byte)'s' << 24);
 
-
     public const int TrueBytesBigEndian = ((byte)'T' << 24) | ((byte)'r' << 16) | ((byte)'u' << 8) | ((byte)'e');
     public const int trueBytesBigEndian = ((byte)'t' << 24) | ((byte)'r' << 16) | ((byte)'u' << 8) | ((byte)'e');
     public const int FalseBytesBigEndian = ((byte)'F' << 24) | ((byte)'a' << 16) | ((byte)'l' << 8) | ((byte)'s');
--- src/Csv/Internal/SequenceReaderExtensions.cs
+++ src/Csv/Internal/SequenceReaderExtensions.cs
@@ -8,37 +8,34 @@ namespace Csv.Internal;
 
 internal static class SequenceReaderExtensions
 {
-      [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static  bool TryRead(ref this SequenceReader<byte> reader, out int value) 
-        {
-            var currentSpan = reader.CurrentSpan;
-            var currentSpanIndex = reader.CurrentSpanIndex;
-            if (currentSpan.Length-currentSpanIndex < sizeof(int))
-                return TryReadMultisegment(ref reader, out value);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static bool TryRead(ref this SequenceReader<byte> reader, out int value)
+    {
+        var currentSpan = reader.CurrentSpan;
+        var currentSpanIndex = reader.CurrentSpanIndex;
+        if (currentSpan.Length - currentSpanIndex < sizeof(int))
+            return TryReadMultisegment(ref reader, out value);
 
-            value = Unsafe.ReadUnaligned<int>(ref Unsafe.Add(ref MemoryMarshal.GetReference(currentSpan),currentSpanIndex));
-            reader.Advance(sizeof(int));
-            return true;
+        value = Unsafe.ReadUnaligned<int>(ref Unsafe.Add(ref MemoryMarshal.GetReference(currentSpan), currentSpanIndex));
+        reader.Advance(sizeof(int));
+        return true;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static bool TryReadMultisegment(ref SequenceReader<byte> reader, out int value)
+    {
+        // Not enough data in the current segment, try to peek for the data we need.
+        int buffer = default;
+        Span<byte> tempSpan = MemoryMarshal.CreateSpan(ref Unsafe.As<int, byte>(ref buffer), 4);
+
+        if (!reader.TryCopyTo(tempSpan))
+        {
+            value = default;
+            return false;
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static  bool TryReadMultisegment(ref SequenceReader<byte> reader, out int value) 
-        {
-
-            // Not enough data in the current segment, try to peek for the data we need.
-            int buffer = default;
-            Span<byte> tempSpan = MemoryMarshal.CreateSpan(ref Unsafe.As<int,byte>(ref buffer), 4);
-
-            if (!reader.TryCopyTo(tempSpan))
-            {
-                value = default;
-                return false;
-            }
-
-            value = Unsafe.ReadUnaligned<int>(ref MemoryMarshal.GetReference(tempSpan));
-            reader.Advance(sizeof(int));
-            return true;
-        }
-    
-    
+        value = Unsafe.ReadUnaligned<int>(ref MemoryMarshal.GetReference(tempSpan));
+        reader.Advance(sizeof(int));
+        return true;
+    }
 }
